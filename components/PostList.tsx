852b389e09d2859177610293ad5c6e04b23eb91c
Graf-Lev-V <legendarlev2004@gmail.com>
2026-05-04 18:21:21 +0300

    update

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -6,16 +6,35 @@ type Post = {
     body: string;
 }
 
+//Pick
+type PostPreview = Pick<Post, 'id' | 'title'>
+//Omit
+type PostDraft = Omit<Post, 'id'>
+//Partial
+type PartialPost = Partial<Post>
+
+const preview: Post = {id: 123, title: 'title', body: 'body'}
+const result: PostPreview = preview;
+console.log(result)
+const draft: Post = {id: 123, title: 'title', body: 'body'}
+const result2: PostDraft = draft;
+console.log(result2)
+const partial: Post = {id: 123, title: 'title', body: 'body'}
+const result3: PartialPost = partial;
+console.log(result3)
+
 export default function PostList() {
 
     const [posts, setPosts] = useState<Post[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
-    
-    
+
+    //custom generic
+    function getLastItem<T>(arr: T[]): T {
+        return arr[arr.length - 1];
+    }
 
     useEffect(() => {
-
         setLoading(true);
         setError(null);
         const controller = new AbortController();
@@ -26,6 +45,7 @@ export default function PostList() {
                 if (!response.ok) throw new Error('Error')
                 const data: Post[] = await response.json()
                 setPosts(data)
+                console.log(getLastItem(data))
             }
             catch (error) {
                 if (error instanceof Error && error.name !== 'AbortError') {
