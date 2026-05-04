035e96c92bad9cb6d561bc26c745d92cc4602c16
Graf-Lev-V <legendarlev2004@gmail.com>
2026-05-04 18:52:13 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import UserCard from '../components/UserCard'
-import UserForm from '../components/UserForm'
-import PostList from '../components/PostList'
+import UserCard from './components/UserCard'
+import UserForm from './components/UserForm'
+import PostList from './components/PostList'
 
 export default function App() {
   return (
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,10 @@
+type UseFetchResult<T> = {
+    data: T | null;
+    loading: boolean;
+    error: string | null;
+}
+
+export default function useFetch() {
+
+    return
+}
